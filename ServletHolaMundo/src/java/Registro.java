6fada16d005c8fa1183448bf641d2987b117a90c
azurim04 <alopezc1904@alumno.ipn.mx>
2021-04-26 18:51:18 -0500

    Hola mundo Servlet 2

--- ServletHolaMundo/src/java/Registro.java
+++ ServletHolaMundo/src/java/Registro.java
@@ -21,12 +21,13 @@ public class Registro extends HttpServlet {
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
      *
-     * @param request servlet request peticiones por parte del cliente
-     * @param response servlet response son las respuestas por parte del servidor
+     * @param request servlet request peticones por parte del cliente
+     * @param response servlet response respuestas por parte del servidor
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    protected void processRequest(HttpServletRequest request, 
+            HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
@@ -34,46 +35,45 @@ public class Registro extends HttpServlet {
             
             //manipular los datos del formulario
             String nom, appat, appmat, correo, ip, iph;
-            int edad, puerto, puertoh;
-            
-            ip = request.getLocalAddr();
-            puerto = request.getLocalPort();
-            
-            iph = request.getRemoteAddr();
-            puertoh = request.getRemotePort();
-            
-            nom = request.getParameter("nombre");
-            appat = request.getParameter("appat");
-            appmat = request.getParameter("appmat");
-            correo = request.getParameter("email");
-            
-            edad = Integer.parseInt(request.getParameter("edad"));
-            
+             int edad, puerto, puertoh;
+             
+             ip = request.getLocalAddr();
+             puerto = request.getLocalPort();
+             iph = request.getRemoteAddr();
+             puertoh = request.getRemotePort();
+             
+             nom = request.getParameter("nombre");
+             appat = request.getParameter("appat");
+             appmat = request.getParameter("appmat");
+             correo = request.getParameter("email");
+             
+             edad = Integer.parseInt(request.getParameter("edad"));
+             
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Registro de usuarios</title>");            
+            out.println("<title>Registro de Usuarios</title>");            
             out.println("</head>");
             out.println("<body>"
-                    + "<br>Tu nombre es: " + nom);
+                    + "<br>Tu nombre es: "+nom);
             out.println("<br>"
-                    + "Tu Apellido Paterno es: " + appat
+                    + "Tu Apellido Paterno es: "+appat
                     + "<br>"
-                    + "Tu Apellido Materno es: " + appmat
+                    + "Tu Apellido Materno es: "+appmat
                     + "<br>"
-                    + "Tu edad es: " + edad
+                    + "Tu edad es: "+edad
                     + "<br>"
-                    + "Tu correo electronico es: " + correo
+                    + "Tu correo electronico es: "+correo
                     + "<br>");
             out.println("<h1>Registro Exitoso</h1>"
                     + "<br>"
-                    + "La IP locar es: " + ip
+                    + "La IP local es: "+ip
                     + "<br>"
-                    + "La IP del host: " + iph 
+                    + "La IP del host es: "+iph
                     + "<br>"
-                    + "Puerto Local: " + puerto
+                    + "Puerto Local: "+puerto
                     + "<br>"
-                    + "Puerto Host: " + puertoh
+                    + "Puerto del Host: "+puertoh
                     + "<br>"
                     + "<a href='index.html'>Regresar al Formulario</a>");
             out.println("</body>");
